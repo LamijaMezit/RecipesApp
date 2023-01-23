54120ad002de4b038f2f47b5e6f2f7ba845cb387
Lamija Mezit <80250132+LamijaMezit@users.noreply.github.com>
2023-01-23 15:54:01 +0100

    Basic operations

---
+++ Recepti-frontend/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule} from "@angular/forms"
+import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule} from "@angular/common/http";
+import { ReceptiComponent } from './recepti/recepti.component';
+import { RouterModule } from '@angular/router';
+import { EditReceptComponent } from './recepti/edit-recept/edit-recept.component';
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReceptiComponent,
+    EditReceptComponent,
+  
+
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'recepti', component: ReceptiComponent},
+      {path: 'edit-recept/:id', component: EditReceptComponent},
+
+
+
+      
+      
+
+      
+    ]),
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
